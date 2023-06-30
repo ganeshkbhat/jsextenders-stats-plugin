b4db59e19f38717908fa06dd33ea5e231c9d211f
Krishnamurthy G B <3424344+ganeshkbhat@users.noreply.github.com>
2023-06-30 10:32:26 +0530

    Create stats.js
    Create stats.js

---
+++ src/stats.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Package: jsextenders-stats-plugin
+ * Author: Ganesh B
+ * Description: jsextenders-stats-plugin for extenders package to cover statistical modules of Descriptive Statistics, Bayesian Classifier, Distributions, Linear Regression
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/jsextenders-stats-plugin
+ * npmjs Link: https://www.npmjs.com/package/jsextenders-stats-plugin
+ * File: .js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+
